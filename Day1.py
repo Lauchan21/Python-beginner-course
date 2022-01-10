543f825962b2dabd9ec6b5574735f4ad13aad28b
Laupho <91590874+Laupho@users.noreply.github.com>
2022-01-10 13:15:47 +0800

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,25 @@
+# Write your code below this line 👇
+print("PRINT FUNCTION - e.g HOW TO WRITE HELLO WORLD")
+print("Hello World!")
+print("HOW TO MAKE SAME TEXT ON SEVERAL COLUMS")
+print("i am the best\ni am the best")
+print ("STRING MANUPILUATION 3 types on how to write")
+print("Hello " + "Angela")
+print("Hello" + " Angela")
+print("Hello" + " " + "Angela")
+#input() will get user input in console
+#Then print() will print the word "Hello" and the user input
+print("INPUT FUNCTION- A PROMPT FOR THR USER e.g. ASK A QUESTION?")
+input("What is your name?")
+print("Hello "+ input("What is your name?") + "!") 
+#number of characters in a user Name or character
+print( len( input("What is your name?")))
+#Variables
+name = input ("What is your name?")
+print (name)
+print ("hello " + (name))
+#Changes variables between 2 (tech create a third variable to switch the 2 first ones)
+c = a
+a = b
+b = c
+#\n also brings you back to a new line (makes the code look better)
