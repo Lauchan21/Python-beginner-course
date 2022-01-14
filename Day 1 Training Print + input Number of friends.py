a38ee82873c75766f5635b45690d21ecf22696ee
Laupho <91590874+Laupho@users.noreply.github.com>
2022-01-14 18:01:10 +0800

    Delete Day 1 Training Print + input Number of friends.py

--- Day 1 Training Print + input Number of friends.py
+++
@@ -1,5 +0,0 @@
-#input () will get user input in console
-#Then print () will print the word and the user input
-
-#Let the computer confirm your number of friends :-)
-print("Indeed you have " + input ("How many friends do you have?") + " friends!!!")
