6f10918a4582128727e05a62d4e281c2a819419a
Laupho <91590874+Laupho@users.noreply.github.com>
2022-01-14 18:00:39 +0800

    Add files via upload

---
+++ Day 1 Training Print + input Number of friends.py
@@ -0,0 +1,5 @@
+#input () will get user input in console
+#Then print () will print the word and the user input
+
+#Let the computer confirm your number of friends :-)
+print("Indeed you have " + input ("How many friends do you have?") + " friends!!!")
