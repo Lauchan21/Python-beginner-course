35b6fd55dd17bec8638ab46ce877b1f6a664b798
Laupho <91590874+Laupho@users.noreply.github.com>
2022-01-14 18:01:18 +0800

    Delete Day 1 Training Print + input Your weight is.py

--- Day 1 Training Print + input Your weight is.py
+++
@@ -1,5 +0,0 @@
-#input () will get user input in console
-#Then print () will print the word and the user input
-
-#Let the computer confirm your weight is correct :-)
-print("I confirm your weight is " + input ("What is your weight in kg?") + "kg!")
