eba0e5f9e16c28f8b97f9dbfb829ee5218248bb7
Laupho <91590874+Laupho@users.noreply.github.com>
2022-01-18 12:59:48 +0800

    Add files via upload

---
+++ Day3 Training IF & ELSE conditions.py
@@ -0,0 +1,9 @@
+print("Hey crypto trader")
+amount = int(input("How much did you invest in$? "))
+
+if amount >= 500:
+  print("You are a rock star!!!!")
+else:
+  print("Have fun staying poor!!!")
+
+
