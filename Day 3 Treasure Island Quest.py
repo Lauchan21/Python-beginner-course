91e0f37757556793833e69eb6244601d4bb20c35
Laupho <91590874+Laupho@users.noreply.github.com>
2022-01-24 13:08:53 +0800

    Add files via upload

---
+++ Day 3 Treasure Island Quest.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+#Write your code below this line 👇
+
+
+street = (input("You can choose 2 streets, choose left or right?\n"))  
+
+if street == "left" or street == "Left":
+    lake = (input("You arrive in front of a lake, do you swim or wait for a boat to come?\n"))
+    if lake == "wait" or lake == "Wait":
+      door = (input("The boat brings you over the lake, you have a blue, red and yellow door, choose one?\n"))
+      if door == "yellow" or door == "Yellow":
+        print ("Congratulation you found the treasure!!!")
+      else:
+        print ("The door is locked and you have no keys, game over.")
+
+    else:
+      print("You forgot you couldnt swim, you died...Game over.")
+
+else:
+  print("You got lost in the woods, game over.")
+
