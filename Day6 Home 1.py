b494eeee39ea841757ca606fc96b011daa9d0137
Laupho <91590874+Laupho@users.noreply.github.com>
2022-02-07 18:17:43 +0800

    Add files via upload

---
+++ Day6 Home 1.py
@@ -0,0 +1,17 @@
+while not at_goal():
+    if wall_in_front():
+        turn_left()
+    else:
+        move()
+
+
+
+
+
+
+
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
