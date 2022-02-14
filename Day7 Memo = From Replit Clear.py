b4ff8290e97abeb5dbd238cf19904804f8b96761
Lauchan21 <91590874+Lauchan21@users.noreply.github.com>
2022-02-14 15:39:12 +0800

    Add files via upload

---
+++ Day7 Memo = From Replit Clear.py
@@ -0,0 +1,5 @@
+#Clear console text so the new data comes over the previous one (makes the console cleaner/ no backlog of previous guesses)
+from replit import clear
+
+#after used made a guess or chose something
+clear ()
